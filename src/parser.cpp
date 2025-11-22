60b764cb12a23a3415110516b27d7820a5fec530
Cath1yyy <liyuxuan_2025@sjtu.edu.cn>
2025-11-23 01:10:27 +0800

    feat: add conditions to parser logic

--- src/parser.cpp
+++ src/parser.cpp
@@ -76,25 +76,25 @@ Expr List::parse(Assoc &env) {
         return Expr(new Apply(stxs[0]->parse(env), args));
 
     }else{
-    string op = id->s;
-    if (find(op, env).get() != nullptr) {
+        string op = id->s;
+        if (find(op, env).get() != nullptr) {
 
-        //TODO
-        // 在环境中找到变量，解析为函数应用
-        vector<Expr> args;
-        for (size_t i = 1; i < stxs.size(); i++) {
-            args.push_back(stxs[i]->parse(env));
+            //TODO
+            // 在环境中找到变量，解析为函数应用
+            vector<Expr> args;
+            for (size_t i = 1; i < stxs.size(); i++) {
+                args.push_back(stxs[i]->parse(env));
+            }
+            return Expr(new Apply(Expr(new Var(op)), args));
+
         }
-        return Expr(new Apply(Expr(new Var(op)), args));
+            if (primitives.count(op) != 0) {
+            vector<Expr> parameters;
 
-    }
-    if (primitives.count(op) != 0) {
-        vector<Expr> parameters;
-
-        //TODO
-        for (size_t i = 1; i < stxs.size(); i++) {
-            parameters.push_back(stxs[i]->parse(env));
-        }
+            //TODO
+            for (size_t i = 1; i < stxs.size(); i++) {
+                parameters.push_back(stxs[i]->parse(env));
+            }
 
         
         ExprType op_type = primitives[op];
@@ -139,27 +139,269 @@ Expr List::parse(Assoc &env) {
         } else if (op_type == E_LIST) {
             return Expr(new ListFunc(parameters));
         } else if (op_type == E_LT) {
-            //TODO: TO COMPLETE THE LOGIC
+
+            //TODO
+            if (parameters.size() == 2) {
+                return Expr(new Less(parameters[0], parameters[1]));
+            } else {
+                return Expr(new LessVar(parameters));
+            }
+
         } else if (op_type == E_LE) {
-            //TODO: TO COMPLETE THE LOGIC
+
+            //TODO
+            if (parameters.size() == 2) {
+                return Expr(new LessEq(parameters[0], parameters[1]));
+            } else {
+                return Expr(new LessEqVar(parameters));
+            }
+
         } else if (op_type == E_EQ) {
-            //TODO: TO COMPLETE THE LOGIC
+
+            //TODO
+            if (parameters.size() == 2) {
+                return Expr(new Equal(parameters[0], parameters[1]));
+            } else {
+                return Expr(new EqualVar(parameters));
+            }
+
         } else if (op_type == E_GE) {
-            //TODO: TO COMPLETE THE LOGIC
+
+            //TODO
+            if (parameters.size() == 2) {
+                return Expr(new GreaterEq(parameters[0], parameters[1]));
+            } else {
+                return Expr(new GreaterEqVar(parameters));
+            }
+
         } else if (op_type == E_GT) {
-            //TODO: TO COMPLETE THE LOGIC
+
+            //TODO
+            if (parameters.size() == 2) {
+                return Expr(new Greater(parameters[0], parameters[1]));
+            } else {
+                return Expr(new GreaterVar(parameters));
+            }
+
+        } else if (op_type == E_CONS) {
+            if (parameters.size() != 2) {
+                throw RuntimeError("Wrong number of arguments for cons");
+            }
+            return Expr(new Cons(parameters[0], parameters[1]));
+        } else if (op_type == E_CAR) {
+            if (parameters.size() != 1) {
+                throw RuntimeError("Wrong number of arguments for car");
+            }
+            return Expr(new Car(parameters[0]));
+        } else if (op_type == E_CDR) {
+            if (parameters.size() != 1) {
+                throw RuntimeError("Wrong number of arguments for cdr");
+            }
+            return Expr(new Cdr(parameters[0]));
+        } else if (op_type == E_SETCAR) {
+            if (parameters.size() != 2) {
+                throw RuntimeError("Wrong number of arguments for set-car!");
+            }
+            return Expr(new SetCar(parameters[0], parameters[1]));
+        } else if (op_type == E_SETCDR) {
+            if (parameters.size() != 2) {
+                throw RuntimeError("Wrong number of arguments for set-cdr!");
+                }
+            return Expr(new SetCdr(parameters[0], parameters[1]));
+        } else if (op_type == E_NOT) {
+            if (parameters.size() != 1) {
+                throw RuntimeError("Wrong number of arguments for not");
+            }
+            return Expr(new Not(parameters[0]));
+        } else if (op_type == E_AND) {
+            return Expr(new AndVar(parameters));
+        } else if (op_type == E_OR) {
+            return Expr(new OrVar(parameters));
         } else if (op_type == E_AND) {
             return Expr(new AndVar(parameters));
         } else if (op_type == E_OR) {
             return Expr(new OrVar(parameters));
         } else {
-            //TODO: TO COMPLETE THE LOGIC
+
+            //TODO
+            // 其他原始操作，解析为函数应用
+            vector<Expr> args;
+            for (size_t i = 1; i < stxs.size(); i++) {
+                args.push_back(stxs[i]->parse(env));
+            }
+            return Expr(new Apply(Expr(new Var(op)), args));
+
         }
     }
 
     if (reserved_words.count(op) != 0) {
+
+        vector<Expr> parameters;
+        for (size_t i = 1; i < stxs.size(); i++) {
+            parameters.push_back(stxs[i]->parse(env));
+        }
+        
     	switch (reserved_words[op]) {
 			//TODO: TO COMPLETE THE reserve_words PARSER LOGIC
+            case E_BEGIN: {
+                return Expr(new Begin(parameters));
+            }
+            case E_QUOTE: {
+                if (parameters.size() != 1) {
+                    throw RuntimeError("Wrong number of arguments for quote");
+                }
+                // 注意：quote的参数应该是语法对象，不是表达式
+                // 这里需要特殊处理，因为参数不应该被求值
+                if (stxs.size() != 2) {
+                    throw RuntimeError("Wrong number of arguments for quote");
+                }
+                return Expr(new Quote(stxs[1]));
+            }
+            case E_IF: {
+                if (parameters.size() < 2 || parameters.size() > 3) {
+                    throw RuntimeError("Wrong number of arguments for if");
+                }
+                Expr alter_expr = parameters.size() == 3 ? parameters[2] : Expr(nullptr);
+                return Expr(new If(parameters[0], parameters[1], alter_expr));
+            }
+            case E_COND: {
+                vector<vector<Expr>> clauses;
+                for (size_t i = 1; i < stxs.size(); i++) {
+                    List* clause_list = dynamic_cast<List*>(stxs[i].get());
+                    if (!clause_list) {
+                        throw RuntimeError("cond clause must be a list");
+                    }
+                    vector<Expr> clause_exprs;
+                    for (auto& clause_stx : clause_list->stxs) {
+                        clause_exprs.push_back(clause_stx->parse(env));
+                    }
+                    clauses.push_back(clause_exprs);
+                }
+                return Expr(new Cond(clauses));
+            }
+            case E_LAMBDA: {
+                // lambda 函数定义
+                if (parameters.size() < 2) {
+                    throw RuntimeError("Wrong number of arguments for lambda");
+                }
+                // 第一个参数应该是参数列表
+                List* param_list = dynamic_cast<List*>(stxs[1].get());
+                if (!param_list) {
+                    throw RuntimeError("Lambda parameters must be a list");
+                }
+                vector<string> params;
+                for (auto& param_stx : param_list->stxs) {
+                    SymbolSyntax* param_sym = dynamic_cast<SymbolSyntax*>(param_stx.get());
+                    if (!param_sym) {
+                        throw RuntimeError("Lambda parameter must be a symbol");
+                    }
+                    params.push_back(param_sym->s);
+                }
+                // 剩下的参数是函数体
+                if (stxs.size() < 3) {
+                    throw RuntimeError("Lambda must have a body");
+                }
+                // 解析所有函数体表达式
+                vector<Expr> body_exprs;
+                for (size_t i = 2; i < stxs.size(); i++) {
+                    body_exprs.push_back(stxs[i]->parse(env));
+                }
+                // 如果函数体有多个表达式，用Begin包装，否则直接使用单个表达式
+                Expr body = body_exprs.size() == 1 ? body_exprs[0] : Expr(new Begin(body_exprs));
+                return Expr(new Lambda(params, body));
+            }
+            case E_DEFINE: {
+                if (parameters.size() != 2) {
+                    throw RuntimeError("Wrong number of arguments for define");
+                }
+                SymbolSyntax* var_sym = dynamic_cast<SymbolSyntax*>(stxs[1].get());
+                if (!var_sym) {
+                    throw RuntimeError("Define variable must be a symbol");
+                }
+                return Expr(new Define(var_sym->s, parameters[1]));
+            }
+            case E_LET: {
+                // let 局部绑定
+                if (parameters.size() < 2) {
+                    throw RuntimeError("Wrong number of arguments for let");
+                }
+                // 第一个参数是绑定列表
+                List* bind_list = dynamic_cast<List*>(stxs[1].get());
+                if (!bind_list) {
+                    throw RuntimeError("Let bindings must be a list");
+                }
+                vector<pair<string, Expr>> bindings;
+                for (auto& bind_stx : bind_list->stxs) {
+                    List* bind_pair = dynamic_cast<List*>(bind_stx.get());
+                    if (!bind_pair || bind_pair->stxs.size() != 2) {
+                        throw RuntimeError("Let binding must be a pair (variable value)");
+                    }
+                    SymbolSyntax* var_sym = dynamic_cast<SymbolSyntax*>(bind_pair->stxs[0].get());
+                    if (!var_sym) {
+                        throw RuntimeError("Let variable must be a symbol");
+                    }
+                    Expr value_expr = bind_pair->stxs[1]->parse(env);
+                    bindings.push_back(mp(var_sym->s, value_expr));
+                }
+                // 剩下的参数是函数体
+                if (stxs.size() < 3) {
+                    throw RuntimeError("Let must have a body");
+                }
+                // 解析所有函数体表达式
+                vector<Expr> body_exprs;
+                for (size_t i = 2; i < stxs.size(); i++) {
+                    body_exprs.push_back(stxs[i]->parse(env));
+                }
+                // 如果函数体有多个表达式，用Begin包装，否则直接使用单个表达式
+                Expr body = body_exprs.size() == 1 ? body_exprs[0] : Expr(new Begin(body_exprs));
+                return Expr(new Let(bindings, body));
+            }
+            case E_LETREC: {
+                // letrec 递归局部绑定
+                if (parameters.size() < 2) {
+                    throw RuntimeError("Wrong number of arguments for letrec");
+                }
+                // 第一个参数是绑定列表
+                List* bind_list = dynamic_cast<List*>(stxs[1].get());
+                if (!bind_list) {
+                    throw RuntimeError("Letrec bindings must be a list");
+                }
+                vector<pair<string, Expr>> bindings;
+                for (auto& bind_stx : bind_list->stxs) {
+                    List* bind_pair = dynamic_cast<List*>(bind_stx.get());
+                    if (!bind_pair || bind_pair->stxs.size() != 2) {
+                        throw RuntimeError("Letrec binding must be a pair (variable value)");
+                    }
+                    SymbolSyntax* var_sym = dynamic_cast<SymbolSyntax*>(bind_pair->stxs[0].get());
+                    if (!var_sym) {
+                        throw RuntimeError("Letrec variable must be a symbol");
+                    }
+                    Expr value_expr = bind_pair->stxs[1]->parse(env);
+                    bindings.push_back(mp(var_sym->s, value_expr));
+                }
+                // 剩下的参数是函数体
+                if (stxs.size() < 3) {
+                    throw RuntimeError("Letrec must have a body");
+                }
+                // 解析所有函数体表达式
+                vector<Expr> body_exprs;
+                for (size_t i = 2; i < stxs.size(); i++) {
+                    body_exprs.push_back(stxs[i]->parse(env));
+                }
+                // 如果函数体有多个表达式，用Begin包装，否则直接使用单个表达式
+                Expr body = body_exprs.size() == 1 ? body_exprs[0] : Expr(new Begin(body_exprs));
+                return Expr(new Letrec(bindings, body));
+            }
+            case E_SET: {
+                if (parameters.size() != 2) {
+                    throw RuntimeError("Wrong number of arguments for set!");
+                }
+                SymbolSyntax* var_sym = dynamic_cast<SymbolSyntax*>(stxs[1].get());
+                if (!var_sym) {
+                    throw RuntimeError("Set! variable must be a symbol");
+                }
+                return Expr(new Set(var_sym->s, parameters[1]));
+            }
         	default:
             	throw RuntimeError("Unknown reserved word: " + op);
     	}
@@ -167,5 +409,10 @@ Expr List::parse(Assoc &env) {
 
     //default: use Apply to be an expression
     //TODO: TO COMPLETE THE PARSER LOGIC
+    vector<Expr> args;
+        for (size_t i = 1; i < stxs.size(); i++) {
+            args.push_back(stxs[i]->parse(env));
+        }
+        return Expr(new Apply(Expr(new Var(op)), args));
 }
 }
